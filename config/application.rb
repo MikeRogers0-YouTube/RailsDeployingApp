55923b2e922013b829b22452284710aaeb31199d
Mike Rogers <me@mikerogers.io>
2019-02-24 19:38:56 +0000

    By default, don't generate lots of extra files with generators

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,14 @@ module RailsDeployingApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Don't generate files we don't like.
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.helper false
+      g.view_specs false
+      g.decorator false
+      g.system_tests = nil
+    end
   end
 end
